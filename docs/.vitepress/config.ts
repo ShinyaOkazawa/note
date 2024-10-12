6f64f78c9414142e880166f244b5b7bdcdb1a393
ShinyaOkazawa <fess3idm@gmail.com>
2024-10-12 14:47:12 +0900

    refactor: meta 修正

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,8 +2,15 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: "My Awesome Project",
-  description: "A VitePress Site",
+  lang: 'ja',
+  title: "ザワのメモ帳",
+  description: "自分のメモ帳。日々気づいたことを一口メモの大きさで綴っていく。",
+  head: [
+    ['meta', { name: 'robots', content: 'noindex'}],
+    ['link', { rel: 'icon', href: '/note/images/me.png'}],
+    ['link', { rel: 'apple-touch-icon', sizes: '180x180', href: '/note/images/me.png'}],
+    ['meta', { name: 'apple-mobile-web-app-title', content: 'ザワメモ'}]
+  ],
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
@@ -20,9 +27,5 @@ export default defineConfig({
         ]
       }
     ],
-
-    socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
-    ]
   }
 })
