35d65812f1ac7dbb7875443327839a19eddd1bf4
ShinyaOkazawa <fess3idm@gmail.com>
2024-10-13 21:25:24 +0900

    refactor: 最新の記事を一番上に持ってくる

--- docs/.vitepress/theme/posts.data.ts
+++ docs/.vitepress/theme/posts.data.ts
@@ -1,4 +1,11 @@
 // posts.data.js
 import { createContentLoader } from 'vitepress'
 
-export default createContentLoader('posts/*.md')
+export default createContentLoader('posts/*.md', {
+    transform(data) {
+        return data.sort((a, b) => {
+            // 最新の記事を一番上に持ってくる
+            return +new Date(b.frontmatter.date) - +new Date(a.frontmatter.date)
+        })
+    },
+})
