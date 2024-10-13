3e7a736e569f55a8d30512e13d07eb74c90d0216
ShinyaOkazawa <fess3idm@gmail.com>
2024-10-13 21:19:50 +0900

    refactor: トップ画面に記事を一覧できるようにした

--- docs/.vitepress/theme/Home.vue
+++ docs/.vitepress/theme/Home.vue
@@ -1,8 +1,20 @@
 <script setup lang="ts">
+import { data as posts } from './posts.data'
+import { withBase } from 'vitepress'
+
+function getFormattedDate(dateString: string){
+  const date = new Date(dateString)
+  return date.toLocaleDateString();
+}
 </script>
 
 <template>
-  <div>Home</div>
+  <ul>
+    <li v-for="post of posts">
+      <a :href="withBase(post.url)">{{  post.frontmatter.title }}</a>
+      <span>作成日：{{ getFormattedDate(post.frontmatter.date) }}</span>
+    </li>
+  </ul>
 </template>
 
 <style scoped>
--- docs/.vitepress/theme/Layout.vue
+++ docs/.vitepress/theme/Layout.vue
@@ -7,7 +7,6 @@ const { page, frontmatter } = useData()
 </script>
 
 <template>
-  <h1>Custom Layout</h1>
   <NotFound v-if="page.isNotFound" />
   <Home v-else-if="frontmatter.layout === 'home'" />
   <Page v-else />
