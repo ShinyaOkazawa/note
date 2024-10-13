8f4c6487faf9e4d4deb5e0d9c66f66fb62f44301
ShinyaOkazawa <fess3idm@gmail.com>
2024-10-13 20:06:46 +0900

    feat: カスタムテーマの枠組みを作成

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base: '/note/',
   lang: 'ja',
   title: "ザワのメモ帳",
   description: "自分のメモ帳。日々気づいたことを一口メモの大きさで綴っていく。",
@@ -11,21 +12,4 @@ export default defineConfig({
     ['link', { rel: 'apple-touch-icon', sizes: '180x180', href: '/note/images/me.png'}],
     ['meta', { name: 'apple-mobile-web-app-title', content: 'ザワメモ'}]
   ],
-  themeConfig: {
-    // https://vitepress.dev/reference/default-theme-config
-    nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
-    ],
-
-    sidebar: [
-      {
-        text: 'Examples',
-        items: [
-          { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' }
-        ]
-      }
-    ],
-  }
 })
---
+++ docs/.vitepress/theme/Layout.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { useData } from 'vitepress'
+import NotFound from './NotFound.vue'
+import Home from './Home.vue'
+import Page from './Page.vue'
+const { page, frontmatter } = useData()
+</script>
+
+<template>
+  <h1>Custom Layout</h1>
+  <NotFound v-if="page.isNotFound" />
+  <Home v-else-if="frontmatter.layout === 'home'" />
+  <Page v-else />
+</template>
