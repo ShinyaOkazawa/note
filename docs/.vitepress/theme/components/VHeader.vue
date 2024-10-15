766aed1740a345016cba16952584296709d41776
ShinyaOkazawa <fess3idm@gmail.com>
2024-10-16 01:38:17 +0900

    feat: add header

---
+++ docs/.vitepress/theme/components/VHeader.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <header>
+    <a href="/note/index.html">
+      <img src="/note/icon.png" alt="" width="50" height="50">
+    </a>
+  </header>
+</template>
+
+<style scoped>
+</style>
