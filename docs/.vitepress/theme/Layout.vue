fe4b875242d90bfe84224b0b8ab7a5d53c151537
ShinyaOkazawa <fess3idm@gmail.com>
2024-10-16 01:38:17 +0900

    feat: add header

--- docs/.vitepress/theme/Layout.vue
+++ docs/.vitepress/theme/Layout.vue
@@ -3,10 +3,12 @@ import { useData } from 'vitepress'
 import NotFound from './NotFound.vue'
 import Home from './Home.vue'
 import Page from './Page.vue'
+import VHeader from './components/VHeader.vue'
 const { page, frontmatter } = useData()
 </script>
 
 <template>
+  <VHeader />
   <NotFound v-if="page.isNotFound" />
   <Home v-else-if="frontmatter.layout === 'home'" />
   <Page v-else />
---
+++ docs/.vitepress/theme/components/VHeader.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { withBase, useData } from 'vitepress'
+</script>
+
+<template>
+  <header>
+    <a href="/note/index.html">
+      <img src="../assets/images/icon.svg" alt="" width="50" height="50">
+    </a>
+  </header>
+</template>
+
+<style scoped>
+</style>
